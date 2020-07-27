89b2008253256b71fce139e25b0f25b128ee782a
ZachJHall <zachjohnhall@gmail.com>
2020-07-27 15:54:04 -0500

    Updates

---
+++ precache-manifest.bc0145aa14d3816083f7b4ef08e53a7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41961537376c14c7aba11a6e0e808c95",
+    "url": "/BR-Server-Status-2/index.html"
+  },
+  {
+    "revision": "e77807a390edd77ff388",
+    "url": "/BR-Server-Status-2/static/css/main.4963c6f4.chunk.css"
+  },
+  {
+    "revision": "fcd3f400d55fdd4bbbd9",
+    "url": "/BR-Server-Status-2/static/js/2.0473c21a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BR-Server-Status-2/static/js/2.0473c21a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e77807a390edd77ff388",
+    "url": "/BR-Server-Status-2/static/js/main.c40480b4.chunk.js"
+  },
+  {
+    "revision": "be4d26efc66d261d4ebf",
+    "url": "/BR-Server-Status-2/static/js/runtime-main.2e4e7550.js"
+  },
+  {
+    "revision": "af0947e9b995a10d59b81da6afac5503",
+    "url": "/BR-Server-Status-2/static/media/BR_LOGO_SupernovaGold.af0947e9.svg"
+  }
+]);
