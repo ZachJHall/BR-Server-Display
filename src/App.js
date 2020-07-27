0b574b0c350db8819745211cf4b70d2f9fe4e574
ZachJHall <zachjohnhall@gmail.com>
2020-07-27 15:06:13 -0500

    Added body to app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/header'
+import Body from './components/body'
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Body />
 
 
     </div>
